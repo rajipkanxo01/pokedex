34ea64102e8596f2fa2b11dee36fe3f7079da4e6
Rajib Paudyal <pdlrajip12@gmail.com>
2023-11-09 02:29:22 +0100

    information about pokemon now showing

--- src/components/PokemonInfo.js
+++ src/components/PokemonInfo.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { useQuery } from "react-query";
+import Color from "color-thief-react";
+
 import "../css/PokemonInfo.css";
+import pokemonPhone from "../images/pokephone.png";
 
 export function PokemonInfo({ pokeId }) {
   const {
@@ -12,27 +15,101 @@ export function PokemonInfo({ pokeId }) {
   );
 
   return (
+    /*
     <div className="pokemoninfo-container">
       {isLoading && <p>Loading......</p>} {isError && <p>Error......</p>}
       {!isLoading && pokemon && (
         <>
-          <div className="info-container">
-            <p>{pokemon.name}</p>
+          { <div className="info-container">
+            <p className="name">{pokemon.name}</p>
+            <p>{pokemon.height}</p>
+            <p>{pokemon.weight}</p>
           </div>
           <div className="image-container">
-            {/* <img
-              className="pokemon-image"
-              src={pokemon.sprites.back_default}
-              alt=""
-            /> */}
             <img
               className="pokemon-image"
               src={pokemon.sprites.other["official-artwork"].front_default}
               alt=""
             />
-          </div>
+          </div> }
+          <div className="image-border"></div>
+          <img className="image-container" src={pokemonPhone} alt="" />
         </>
       )}
+    </div>
+*/
+
+    <div className="pokemoninfo-container">
+      {isLoading && <p>Loading......</p>} {isError && <p>Error......</p>}
+      {!isLoading && pokemon && (
+        <Color
+          src={pokemon.sprites.front_default}
+          format="hex"
+          key={pokemon.id}
+          crossOrigin="anonymous"
+        >
+          {({ data: colourData, loading, error }) => (
+            <>
+              <div
+                style={{
+                  background: `${colourData}`,
+                }}
+                className="image-border"
+              >
+                <img
+                  className="pokemon-image"
+                  src={pokemon.sprites.other["official-artwork"].front_default}
+                  alt=""
+                />
+              </div>
+
+              <p className="pokemon-name">{pokemon.name}</p>
+
+              <div className="basic-stats">
+                <p>Height: {pokemon.height}</p>
+                <p>Weight: {pokemon.weight}</p>
+                <p>Basic XP: {pokemon.base_experience}</p>
+                <p>
+                  Abilities:
+                  {pokemon.abilities.map((ability, index) => (
+                    <span>
+                      {ability.ability.name}
+                      {index < pokemon.abilities.length - 1 ? ", " : ""}
+                    </span>
+                  ))}
+                </p>
+              </div>
+
+              <MovesContainer pokemon={pokemon} />
+
+              <p className="pokemon-id">#{pokemon.id}</p>
+              <p className="pokemon-type">
+                {pokemon.types.map((typeData, index) => (
+                  <p>
+                    {typeData.type.name}
+                    {index < pokemon.types.length - 1 ? ", " : ""}
+                  </p>
+                ))}
+              </p>
+
+              <img className="pokephone-image" src={pokemonPhone} alt=""></img>
+            </>
+          )}
+        </Color>
+      )}
+    </div>
+  );
+}
+
+function MovesContainer({ pokemon }) {
+  const moves = pokemon.moves.map((move) => move.move.name);
+  return (
+    <div className="moves-container">
+      {Array.from({ length: 10 }).map((_, index) => (
+        <div key={index} className="box">
+          <p>{moves.length > 0 ? moves[index % moves.length] : ""}</p>
+        </div>
+      ))}
     </div>
   );
 }
