fe5adf7edc587ba89ee2fa3556441132fd910286
Rajib Paudyal <pdlrajip12@gmail.com>
2023-11-10 01:49:06 +0100

    - routing now works - created background component .. so background is same for all pages

--- src/index.js
+++ src/index.js
@@ -1,50 +1,33 @@
-import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import "../src/css/index.css";
-import { Navbar } from "./components/Navbar";
-import { Sidebar } from "./components/PokemonSidebar";
-import { PokemonInfo } from "./components/PokemonInfo";
+import { RouterProvider, createHashRouter } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import { Root } from "./routes/Root";
 
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function App() {
-  const [pokemonId, setPokemonId] = useState(0);
-
-  function handleCardClick(pokeId) {
-    setPokemonId(pokeId);
-  }
-
-  return (
-    <div>
-      <BackgroundContainer />
-      <Overlay />
-      <Navbar />
-      <Body pokemonId={pokemonId} handleCardClick={handleCardClick} />
-    </div>
-  );
-}
-
-function BackgroundContainer() {
-  return <div className="background-container"></div>;
-}
-
-function Overlay() {
-  return <div className="overlay"></div>;
-}
-
-function Body({ pokemonId, handleCardClick }) {
-  return (
-    <div className="body">
-      <PokemonInfo pokeId={pokemonId} />
-      <Sidebar handleClick={handleCardClick} />
-    </div>
-  );
-}
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
 
 root.render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <RouterProvider router={router} />
   </QueryClientProvider>
 );
